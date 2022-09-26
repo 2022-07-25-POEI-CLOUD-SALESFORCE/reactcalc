e6878c18581195495839c19607507b6fd3cf269f
Bruno-TAVERNIER <bruno@tavernier.eu>
2022-09-26 15:14:34 +0200

    squelette

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+// à exporter si non fait dans la déclaration du composant: export default App;
